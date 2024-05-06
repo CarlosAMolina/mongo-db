ba264e4f425c234d640fbbc05f71976cdf9b32dc
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-06 20:40:13 +0200

    Add dependency, uncomment code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-// const mongoose = require('mongoose');
-//
-//
-//const connect = () => {
-//    return mongoose.connect("mongodb://localhost:27017/dbname");
-//}
+const mongoose = require('mongoose');
+
+
+// If the datbase name does not exist, it will be created.
+const connect = () => {
+    return mongoose.connect("mongodb://localhost:27017/dbname");
+}
+
+// Creeate a schema.
+const student = new mongoose.Schema({
+    firstName: String
+})
+
+// First argument is the collection.
+const Student = mongoose.model('student', student)
+
 function sum_1(number) {
   return number + 1;
 }
+
 
 export const exportedForTesting = {
   sum_1,
